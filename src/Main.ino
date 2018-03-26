7f5511d80b8f28b26d7f3b19273caa788121dc6a
Fodé Guirassy <fodeguipro@gmail.com>
2018-03-26 12:01:40 +0200

    dispaly qrcode through oled boilerplate code

--- src/Main.ino
+++ src/Main.ino
@@ -3,18 +3,31 @@
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>
 
+#include <Wire.h>
+#include "SSD1306.h"
+#include <qrcode.h>
+
+//21 & 22 are ESP connection pins
+//03xc is device I2C address
+
+SSD1306 display(0x3c, 21, 22);
+QRcode qrcode (&display);
+
 void setup() {
-
   Serial.begin(115200);
-
   WiFiManager wifiManager;
 
   wifiManager.resetSettings();
   wifiManager.setDebugOutput(true);
-
   wifiManager.autoConnect("FlashLuv","FlashLuv");
-
   Serial.println("CONNECTION SUCCESSFULLY DONE");
+
+  display.init();
+  display.display();
+
+  qrcode.init();
+  qrcode.create("Hello World");
+
 }
 
 void loop() {
