6dbe8a1a5d5ec78ed0d8ce417469ebb08d8b755f
hGxL <henri.m.gil@gmail.com>
2018-06-23 11:37:21 +0200

    Functions Heart spectrum Led

--- src/Main/Main.ino
+++ src/Main/Main.ino
@@ -2,10 +2,20 @@
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>
+#include <LEDMatrixDriver.hpp>
 
 #include <Wire.h>
 #include "SSD1306.h"
 #include <qrcode.h>
+
+//LED HEART
+const uint8_t LEDMATRIX_CS_PIN = 15;
+const int LEDMATRIX_WIDTH = 31;
+const int LEDMATRIX_HEIGHT = 7;
+const int LEDMATRIX_SEGMENTS = 4;
+LEDMatrixDriver lmd(LEDMATRIX_SEGMENTS, LEDMATRIX_CS_PIN);
+int values[LEDMATRIX_WIDTH];
+
 
 //21 & 22 are ESP connection pins
 //03xc is device I2C address
@@ -28,12 +38,64 @@ void setup() {
   qrcode.init();
   qrcode.create("Hello World");
 
+  //LEAD HEART
+  lmd.setEnabled(true);
+  lmd.setIntensity(2);
+  
+  for(int i=0; i<LEDMATRIX_WIDTH; i++){
+     values[i] = 1;
+  }
   
 }
 
 void loop() {
 
   Serial.println("HELLO WORLD");
+  
+  insertNewVal( random(100,999) );
+  delay(100); 
 
-  delay(2000);
 }
+
+void insertNewVal(int newVal){
+    int cast_ = newVal/100;
+    if (cast_ > LEDMATRIX_HEIGHT ){
+      cast_ = LEDMATRIX_HEIGHT;
+      }
+      
+    for(int i= LEDMATRIX_WIDTH ; i > 0; i--) {
+         values[i] =  values[i-1];
+     }
+    
+    values[0] = cast_;
+
+     setGraph();
+ }
+
+void setGraph(){
+    for(int i=0; i<LEDMATRIX_WIDTH; i++) {
+      delay(10);
+      turnOn(i,i,values[i]);
+      delay(10);
+    }
+ }
+
+void turnOn(int x, int y, int h){
+
+   for(int i=0; i<LEDMATRIX_HEIGHT; i++){
+     lmd.setPixel(x, i, false);
+     lmd.display();
+    }
+
+    for (int i = 0; i < h; i++){
+      lmd.setPixel(x, i, true);
+      lmd.display();
+    }
+   
+ }
+
+
+
+
+
+
